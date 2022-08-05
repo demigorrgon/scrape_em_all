ae2f2429835d1814533dd85646fbb9fb7931f5d1
Valentyn Slivko <metallistvalon@gmail.com>
2022-08-05 21:50:34 +0300

    Add celery config

--- scrape_em_all/config.py
+++ scrape_em_all/config.py
@@ -2,8 +2,10 @@ import os
 
 import mongoengine
 import pytz
-import redis
+
+# import redis
 from aiogram import Bot, Dispatcher
+from celery import Celery
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,3 +18,4 @@ dispatcher = Dispatcher(bot)
 # r = redis.Redis(host=os.environ.get("REDIS_HOST", port=6379, db=0))
 
 mongo_connection = mongoengine.register_connection(alias="core", name="scrape_em_db")
+app = Celery("scrape_em_all", broker="redis://0.0.0.0:6379//0")
