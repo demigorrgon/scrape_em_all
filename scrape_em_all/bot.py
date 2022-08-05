fbb2267d08316d5249a465250b27b43a163b4a49
Valentyn Slivko <metallistvalon@gmail.com>
2022-08-05 15:11:39 +0300

    Add basic commands/validations

--- scrape_em_all/bot.py
+++ scrape_em_all/bot.py
@@ -1,20 +1,20 @@
 import logging
-import os
 
-from aiogram import Bot, Dispatcher, executor, types
+import mongoengine
+from aiogram import executor, types
 from aiogram.dispatcher.filters import Text
-from dotenv import load_dotenv
+
+from scrape_em_all.config import dispatcher
+from scrape_em_all.helpers import register_new_user
+from scrape_em_all.models import User
 
 logging.basicConfig(level=logging.INFO)
-
-load_dotenv()
-
-bot = Bot(token=os.environ.get("BOT_TOKEN"))
-dispatcher = Dispatcher(bot)
 
 
 @dispatcher.message_handler(commands=["start", "help"])
 async def greet(message: types.Message):
+    # try except
+
     commands_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
     start_parsing_button = types.KeyboardButton(text="Start parsing for the 1st time")
     subscribe_to_updates = types.KeyboardButton(
@@ -22,17 +22,26 @@ async def greet(message: types.Message):
     )
     commands_keyboard.add(start_parsing_button)
     commands_keyboard.add(subscribe_to_updates)
+    if not register_new_user(message):
+        await message.answer(
+            "You've already started conversation with me, please select parsing choices.",
+            reply_markup=commands_keyboard,
+        )
 
-    await message.answer(
-        "Heyo! I'm a Scrape'em all bot. I will help you with finding desired jobs in Ukrainian IT segment.\nCurrently supported: djinni.co, jobs.dou.ua, robota.ua, work.ua",
-        reply_markup=commands_keyboard,
-    )
+    else:
+        await message.answer(
+            "Heyo! I'm a Scrape'em all bot. I will help you with finding desired jobs in Ukrainian IT segment.\nCurrently supported: djinni.co, jobs.dou.ua, robota.ua, work.ua",
+            reply_markup=commands_keyboard,
+        )
 
 
 @dispatcher.message_handler(Text(equals="Start parsing for the 1st time"))
 async def handle_first_time_parsing(message: types.Message):
+    options_after_parsing = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    some_button = types.KeyboardButton(text="DEBUG")
+    options_after_parsing.add(some_button)
     await message.reply(
-        "Started parsing.... (static)", reply_markup=types.ReplyKeyboardRemove()
+        "Started parsing.... (static)", reply_markup=options_after_parsing
     )
 
 
@@ -44,5 +53,13 @@ async def subscribe_to_scheduled_parsing(messsage: types.Message):
     )
 
 
+@dispatcher.message_handler(Text(equals="User"))
+async def debug_queries(message: types.Message):
+    user_data = User.objects(username=message.chat.username).first()
+    print(user_data)
+    await message.reply(f"{user_data}")
+
+
 if __name__ == "__main__":
+    mongoengine.register_connection(alias="core", name="scrape_em_db")
     executor.start_polling(dispatcher, skip_updates=True)
