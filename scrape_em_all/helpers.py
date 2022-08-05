77377326fe54511be79b1874a99a56222ab14389
Valentyn Slivko <metallistvalon@gmail.com>
2022-08-05 21:50:44 +0300

    Add date in ukrainian to datetime helper function

--- scrape_em_all/helpers.py
+++ scrape_em_all/helpers.py
@@ -1,9 +1,14 @@
-from aiogram import types
+from copy import deepcopy
+from datetime import datetime, timedelta
 
+from aiogram import types
+from dateutil.relativedelta import relativedelta
+
+# from scrape_em_all.config import tz
 from scrape_em_all.models import User
 
 
-def is_user_registered(username: str):
+def is_user_registered(username: str) -> True | False:
     user = User.objects(username=username).first()
     if user:
         return True
@@ -22,3 +27,36 @@ def register_new_user(message: types.Message) -> User | None:
     user.parsed_vacancies = []
     user.save()
     return user
+
+
+def date_in_ukrainian_to_datetime(date_to_convert: str) -> datetime:
+    # current_year_object is there to copy it and butcher him later
+    # to create month objects of current year
+    current_year_object = datetime.today()
+    dates = {
+        "сьогодні": datetime.today(),
+        "вчора": datetime.today() - timedelta(days=1),
+    }
+    months_in_ukrainian = [
+        "січня",
+        "лютого",
+        "березня",
+        "квітня",
+        "травня",
+        "червня",
+        "липня",
+        "серпня",
+        "вересня",
+        "жовтня",
+        "листопада",
+        "грудня",
+    ]
+    # generating dict values instead of hardcoding into huge dict
+    for num, month in enumerate(months_in_ukrainian):
+        dates[month] = deepcopy(current_year_object) + relativedelta(month=num + 1)
+
+    day_from_input, month_from_input = date_to_convert.split(" ")
+    tranlate_to_datetime = dates[month_from_input] + relativedelta(
+        day=int(day_from_input)
+    )
+    return tranlate_to_datetime.strftime("%d-%m-%Y")
