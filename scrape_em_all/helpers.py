235a50d42521aeae338018fd08cca0512613fae2
Valentyn Slivko <metallistvalon@gmail.com>
2022-08-07 19:05:42 +0300

    Add helper functions

--- scrape_em_all/helpers.py
+++ scrape_em_all/helpers.py
@@ -5,7 +5,7 @@ from aiogram import types
 from dateutil.relativedelta import relativedelta
 
 # from scrape_em_all.config import tz
-from scrape_em_all.models import User
+from scrape_em_all.models import BaseVacancy, DjinniVacancies, User
 
 
 def is_user_registered(username: str) -> True | False:
@@ -54,9 +54,36 @@ def date_in_ukrainian_to_datetime(date_to_convert: str) -> datetime:
     # generating dict values instead of hardcoding into huge dict
     for num, month in enumerate(months_in_ukrainian):
         dates[month] = deepcopy(current_year_object) + relativedelta(month=num + 1)
+    try:
+        day_from_input, month_from_input = date_to_convert.split(" ")
+        tranlate_to_datetime = dates[month_from_input] + relativedelta(
+            day=int(day_from_input)
+        )
+    except ValueError:
+        tranlate_to_datetime = dates[date_to_convert]
+    return tranlate_to_datetime
 
-    day_from_input, month_from_input = date_to_convert.split(" ")
-    tranlate_to_datetime = dates[month_from_input] + relativedelta(
-        day=int(day_from_input)
-    )
-    return tranlate_to_datetime.strftime("%d-%m-%Y")
+
+def get_user_or_exception(username: str) -> User:
+    user = User.objects.get(username=username)
+    if not user:
+        raise Exception("Such user does not exist")
+
+    return user
+
+
+def check_if_parsed_entry_exists_in_db(
+    vacancies_model: DjinniVacancies, user: User, parsed_data: tuple
+) -> True | False:
+    """
+    Returns True if parsed_data exists in db, False otherwise.
+    Function is querying document by page link (it is unique to every parsed element)
+    @params: model = EmbeddedDocument instance according to the parsed site
+             parsed_data = tuple of parsed_values in fixed schema, ie link would be parsed_data[1]
+    Function should be extended for each website accordingly
+    """
+    if vacancies_model.__name__ == "DjinniVacancies":
+        data_exists = DjinniVacancies.objects(parsed_by=user, link=parsed_data[1])
+        if data_exists:
+            return True
+        return False
