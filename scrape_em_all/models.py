19c2ceb916cad4734bf41cea9341f6ea5e25972b
Valentyn Slivko <metallistvalon@gmail.com>
2022-08-07 19:03:35 +0300

    Update models

--- scrape_em_all/models.py
+++ scrape_em_all/models.py
@@ -7,19 +7,6 @@ from scrape_em_all.config import tz
 datetime_with_tz = datetime.datetime.now(tz=tz)
 
 
-class VacanciesPerSpecialization(mongoengine.EmbeddedDocument):
-    selected_language = mongoengine.StringField()
-    djinni_vacancies = mongoengine.ListField()
-    dou_vacancies = mongoengine.ListField()
-    robota_vacancies = mongoengine.ListField()
-    workua_vacancies = mongoengine.ListField()
-
-    meta = {
-        "db_alias": "core",
-        "collection": "vacancies",
-    }
-
-
 class User(mongoengine.Document):
     username = mongoengine.StringField(required=True)
     telegram_id = mongoengine.StringField(required=True)
@@ -27,8 +14,52 @@ class User(mongoengine.Document):
     first_interaction_with_bot = mongoengine.DateTimeField(
         default=datetime_with_tz,
     )
-    parsed_vacancies = mongoengine.EmbeddedDocumentListField(VacanciesPerSpecialization)
+
     meta = {
         "db_alias": "core",
         "collection": "user",
     }
+
+
+class BaseVacancy(mongoengine.Document):
+    parsed_by = mongoengine.ReferenceField(User)
+    title = mongoengine.StringField()
+    link = mongoengine.StringField()
+    short_description = mongoengine.StringField()
+    ad_posted_at = mongoengine.DateField()
+
+    meta = {
+        "db_alias": "core",
+        "ordering": ["-ad_posted_at"],
+        "allow_inheritance": True,
+        "abstract": True,
+    }
+
+
+class DjinniVacancies(BaseVacancy):
+    meta = {
+        "db_alias": "core",
+        "collection": "djinni",
+        "ordering": ["-ad_posted_at"],
+    }
+
+
+# class DouVacancies(DjinniVacancies):
+#     meta = {
+#         "db_alias": "core",
+#         "collection": "dou",
+#     }
+
+
+# class RobotaVacancies(DjinniVacancies):
+#     meta = {
+#         "db_alias": "core",
+#         "collection": "robota",
+#     }
+
+
+# class WorkVacancies(DjinniVacancies):
+#     meta = {
+#         "db_alias": "core",
+#         "collection": "workua",
+#     }
