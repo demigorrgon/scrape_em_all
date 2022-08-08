62538dc93e5c320472bf8c40cd25987e72d9c2d5
Valentyn Slivko <metallistvalon@gmail.com>
2022-08-08 21:29:06 +0300

    Remake tasks. New tasks are infinite loops that are supposed to be killed on users' /break command

--- scrape_em_all/tasks.py
+++ scrape_em_all/tasks.py
@@ -1,10 +1,11 @@
 import asyncio
+import time
 
 # from celery.schedules import crontab
 from celery.utils.log import get_task_logger
 
 from scrape_em_all.config import app
-from scrape_em_all.scraper import DjinniScraper
+from scrape_em_all.scraper import DjinniScraper, DouScraper
 
 logger = get_task_logger(__name__)
 
@@ -12,17 +13,18 @@ logger = get_task_logger(__name__)
 @app.task(name="djinni_parsing")
 def scheduled_djinni_parsing(telegram_username):
     logger.info(f"Djinni parsing started by {telegram_username}")
-    scraper = DjinniScraper(telegram_username=telegram_username)
-    asyncio.run(scraper.fetch())
-    logger.info(f"Djinni parsing finished, user:{telegram_username}")
-    # app.conf.beat_schedule["djinni_parsing"]["args"] = tuple([telegram_username])
-    # print(app.conf.beat_schedule)
-    return 200
+    while True:
+        scraper = DjinniScraper(telegram_username=telegram_username)
+        asyncio.run(scraper.fetch())
+        logger.info(f"Djinni parsing finished, user:{telegram_username}")
+        time.sleep(60)
 
 
-app.conf.beat_schedule = {
-    "djinni-parsing": {
-        "task": "djinni_parsing",
-        "schedule": 120.0,
-    },
-}
+@app.task(name="dou_parsing")
+def scheduled_dou_parsing(telegram_username: str) -> None:
+    logger.info(f"Dou parsing started by {telegram_username}")
+    while True:
+        dou_scraper = DouScraper(telegram_username=telegram_username)
+        asyncio.run(dou_scraper.fetch())
+        logger.info(f"Dou parsing finished, user:{telegram_username}")
+        time.sleep(60)
