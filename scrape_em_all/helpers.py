d7a5b52d25ca9aec8652cbf35bc5e278f5a05915
Valentyn Slivko <metallistvalon@gmail.com>
2022-08-08 21:31:44 +0300

    Add task manager object that stores and manipulates active celery tasks of current user

--- scrape_em_all/helpers.py
+++ scrape_em_all/helpers.py
@@ -5,7 +5,26 @@ from aiogram import types
 from dateutil.relativedelta import relativedelta
 
 # from scrape_em_all.config import tz
-from scrape_em_all.models import BaseVacancy, DjinniVacancies, User
+from scrape_em_all.models import BaseVacancy, User
+
+
+class CeleryTaskManager:
+    __storage = {}
+
+    def __init__(self, username: str) -> None:
+        self.username = username
+
+    def add_to_storage(self, task_name: str, task_id):
+        self.__class__.__storage[task_name] = task_id
+
+        print(f"Added {task_id} to storage")
+        print(f"{self.username} tasks:", self.__class__.__storage)
+
+    def retrieve_task(self, key):
+        return self.__class__.__storage[key]
+
+    def clear_tasks_from_storage(self):
+        self.__storage = {}
 
 
 def is_user_registered(username: str) -> True | False:
@@ -24,7 +43,6 @@ def register_new_user(message: types.Message) -> User | None:
     user = User()
     user.username = username
     user.telegram_id = str(user_tg_id)
-    user.parsed_vacancies = []
     user.save()
     return user
 
@@ -55,7 +73,16 @@ def date_in_ukrainian_to_datetime(date_to_convert: str) -> datetime:
     for num, month in enumerate(months_in_ukrainian):
         dates[month] = deepcopy(current_year_object) + relativedelta(month=num + 1)
     try:
-        day_from_input, month_from_input = date_to_convert.split(" ")
+        date_string = date_to_convert.split(" ")
+        if (
+            len(date_string) == 2
+        ):  # example: 7 серпня will come in 'if', 7 серпня 2022 wont
+            day_from_input, month_from_input = date_string
+            tranlate_to_datetime = dates[month_from_input] + relativedelta(
+                day=int(day_from_input)
+            )
+            return tranlate_to_datetime
+        day_from_input, month_from_input = date_string[:-1]  # cutting year out
         tranlate_to_datetime = dates[month_from_input] + relativedelta(
             day=int(day_from_input)
         )
@@ -73,17 +100,15 @@ def get_user_or_exception(username: str) -> User:
 
 
 def check_if_parsed_entry_exists_in_db(
-    vacancies_model: DjinniVacancies, user: User, parsed_data: tuple
+    vacancies_model: BaseVacancy, user: User, parsed_data: tuple
 ) -> True | False:
     """
     Returns True if parsed_data exists in db, False otherwise.
     Function is querying document by page link (it is unique to every parsed element)
-    @params: model = EmbeddedDocument instance according to the parsed site
+    @params: model = Document instance according to the parsed site
              parsed_data = tuple of parsed_values in fixed schema, ie link would be parsed_data[1]
-    Function should be extended for each website accordingly
     """
-    if vacancies_model.__name__ == "DjinniVacancies":
-        data_exists = DjinniVacancies.objects(parsed_by=user, link=parsed_data[1])
-        if data_exists:
-            return True
-        return False
+    data_exists = vacancies_model.objects(parsed_by=user, link=parsed_data[1])
+    if data_exists:
+        return True
+    return False
